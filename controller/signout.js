c21182075b3c55b1b3d31b1918d418746859cf18
Aukik Aurnab <aukikaurnabx@gmail.com>
2022-04-02 04:59:00 +0600

    gg

--- controller/signout.js
+++ controller/signout.js
@@ -1,13 +1,15 @@
-const express = require('express')
+const express = require("express")
 
 let router = express.Router()
 
-router.get('/signout', (req, res) => {
+router.get("/signout", (req, res) => {
   if (req.session.username)
     return req.session.destroy(err => {
       if (err) return console.log(err)
-      res.redirect('/')
+      res.redirect("/")
     })
-  else return res.redirect('/signin')
+  else
+    //gg
+    return res.redirect("/signin")
 })
 module.exports = router
